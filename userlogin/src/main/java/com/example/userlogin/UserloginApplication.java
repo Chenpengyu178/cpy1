ef3d0c9ad9e38fa1bf5e77fa154e5d7e09e08753
Chenpengyu178 <2543866684@qq.com>
2025-07-03 18:19:51 +0800

    Add files via upload

--- userlogin/src/main/java/com/example/userlogin/UserloginApplication.java
+++ userlogin/src/main/java/com/example/userlogin/UserloginApplication.java
@@ -1,9 +1,11 @@
 package com.example.userlogin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.userlogin.dao")
 public class UserloginApplication {
 
     public static void main(String[] args) {
